f6f2750700e58f38c6b0e1c9697124500eae8170
LuqiMarino <lucasmarino96@hotmail.com>
2021-10-07 17:54:31 -0300

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBAPMHRFrXBQyRRRIjHZBG_IblVkLTczOw",
+    authDomain: "primerparciallabo4lucasmarino.firebaseapp.com",
+    projectId: "primerparciallabo4lucasmarino",
+    storageBucket: "primerparciallabo4lucasmarino.appspot.com",
+    messagingSenderId: "120334342158",
+    appId: "1:120334342158:web:146ffda95577095439c45f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBAPMHRFrXBQyRRRIjHZBG_IblVkLTczOw",
+    authDomain: "primerparciallabo4lucasmarino.firebaseapp.com",
+    projectId: "primerparciallabo4lucasmarino",
+    storageBucket: "primerparciallabo4lucasmarino.appspot.com",
+    messagingSenderId: "120334342158",
+    appId: "1:120334342158:web:146ffda95577095439c45f"
+  }
+  
 };
 
 /*
